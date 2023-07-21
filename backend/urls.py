c822887a70158fdc5866ed9d8f3a134351067522
ssojungg <kawy.sojung@gmail.com>
2023-07-21 16:21:30 +0900

    feature: User/login add 2차 수정/swagger 연동

--- backend/urls.py
+++ backend/urls.py
@@ -16,11 +16,32 @@ Including another URLconf
 # backend/urls.py
 
 # backend/urls.py
+from rest_framework import permissions
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="remember+ API",
+        default_version='v1',
+        description="remember+ API 문서",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="kawy.sojung@gmail.com"),
+        license=openapi.License(name="kawksojung"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('neo_db.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('USER/', include('neo_db.urls'))
 ]
+
 #
