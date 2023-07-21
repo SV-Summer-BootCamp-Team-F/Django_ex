c0aebffc60ea7f8b29a2855264b292e141243211
hajaemin <penloo@naver.com>
2023-07-21 17:29:58 +0900

    # feat: 새로운 기능 추가 # fix: 버그 수정 # docs: 문서 수정 test: 코드 추가 # refact: 코드 리팩토링 # style: 코드 formatting, 세미콜론 누락, 코드 자체의 변경이 없는 경우 # chore: 패키지 매니저 수정, 그 외 기타 수정 ex) .gitignore # design: CSS 등 사용자 UI 디자인 변경  comment: 주석 추가 # rename: 파일 또는 폴더 명을 수정하거나 옮기는 작업만인 경우 # remove: 파일을 삭제하는 작업만 수행한 경우 # !BREAKING CHANGE: 커다란 API 변경의 이유 # !HOTFIX: 급하게 치명적인 버그를 고쳐야 하는 경우

--- backend/urls.py
+++ backend/urls.py
@@ -22,8 +22,8 @@ from django.urls import path, include, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
+schema_view = get_schema_view(                  #  API 스키마를 만들기 위한 뷰를 생성하는 데 사용,Swagger UI와 연동되어 API 문서를 제공하고 시각적으로 보여줌
+    openapi.Info(                               #  API의 기본 정보를 설정
         title="remember+ API",
         default_version='v1',
         description="remember+ API 문서",
@@ -31,17 +31,17 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email="kawy.sojung@gmail.com"),
         license=openapi.License(name="kawksojung"),
     ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
+    public=True,                                #  API 스키마가 공개되도록 설정
+    permission_classes=[permissions.AllowAny],  #  누구나 API 스키마를 조회할 수 있도록 허용
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('neo_db.urls')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('USER/', include('neo_db.urls'))
+    path('admin/', admin.site.urls),             # 관리자 사이트에 대한 URL 패턴
+    path('', include('neo_db.urls')),            # 빈경로 URL 패턴으로, neo_db.urls로 시작하는 URL을 include합니다. 이는 neo_db 애플리케이션의 URL 설정을 추가하라는 의미
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'), # swagger UI의 JSON 또는 YAML 스키마를 조회하기 위한 URL
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),                 #  /swagger/ 경로에 대한 URL 패턴으로, Swagger UI를 사용하여 API 스키마를 조회
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # Redoc UI를 사용하여 API 스키마를 조회할 수 있도록 설정
+    path('USER/', include('neo_db.urls'))   #neo_db.urls로 시작하는 URL을 include,  /USER/ 경로를 처리하기 위해 해당 애플리케이션의 URL 설정을 추가하라는 의미
 ]
 
 #
--- neo_db/serializers.py
+++ neo_db/serializers.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 import bcrypt
 from rest_framework import serializers
-from .models import User
+from .models import USER , CARD, HAS_RELATION
 
 SECRET_KEY = 'django-insecure-ei+d5(j38wpe9abp-nnc3q^sjc+!5bzs-$i=n!-9jj22gem$#w'  # Change this with your own secret key
 
@@ -14,12 +14,43 @@ class UserSerializer(serializers.Serializer):
     user_photo = serializers.CharField(max_length=5000, allow_blank=True)
     is_user = serializers.BooleanField()
     created_at = serializers.DateTimeField()
-#안녕
+
+    # 모델을 JSON 형태로 변환
     class Meta:
         model = User
         fields = (
             'user_name', 'user_email', 'password', 'phone_num', 'user_photo',
             'is_user', 'created_at', 'update_at', 'delete_at')
+
+
+class CardSerializer(serializers.ModelSerializer):
+    card_name = serializers.CharField()
+    card_email = serializers.EmailField()
+    card_info = serializers.CharField()
+    card_photo = serializers.CharField()
+    created_at = serializers.DateTimeField()
+    update_at = serializers.DateTimeField(allow_null=True)
+    class Meta:
+        model = CARD
+        fields = (
+            'card_name', 'card_email', 'card_info', 'card_photo',
+            'created_at', 'update_at'
+        )
+
+class RelationshipSerializer(serializers.ModelSerializer):
+    relation_name = serializers.CharField()
+    memo = serializers.CharField()
+    created_at = serializers.DateTimeField()
+    card_id = serializers.CharField(source='card.id')
+    card_name = serializers.CharField(source='card.card_name')
+    user_photo = serializers.CharField(source='card.user_photo')
+
+    class Meta:
+        model = HAS_RELATION
+        fields = (
+            'relation_name', 'memo', 'created_at', 'card_id', 'card_name', 'user_photo'
+        )
+
 
 
 class LoginSerializer(serializers.Serializer):
