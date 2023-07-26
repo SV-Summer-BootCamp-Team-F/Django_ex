b97319c8425c71e704629847b606dd4481bbaac4
hajaemin <penloo@naver.com>
2023-07-26 21:01:07 +0900

    feat: 명함 정보 불러오기have 연결 # fix: 버그 수정 # docs: 문서 수정 test: 도커 코드 추가 # refact: 코드 리팩토링 # style: 코드 formatting, 세미콜론 누락, 코드 자체의 변경이 없는 경우 # chore: 패키지 매니저 수정, 그 외 기타 수정 ex) .gitignore # design: CSS 등 사용자 UI 디자인 변경 # comment:세팅관련 주석 추가 # rename: 파일 또는 폴더 명을 수정하거나 옮기는 작업만인 경우 # remove: 파일을 삭제하는 작업만 수행한 경우 # !BREAKING CHANGE: 커다란 API 변경의 이유 # !HOTFIX: 급하게 치명적인 버그를 고쳐야 하는 경우

--- neo_db/views.py
+++ neo_db/views.py
@@ -251,11 +251,11 @@ class CardAddView(views.APIView):
                 session.run("""
                     CREATE (card:Card {
                        uid: $card_uid,
-                        name: $card_name,
-                        email: $card_email,
-                        phone: $card_phone,
-                        intro: $card_intro,
-                        photo: $card_photo,
+                        card_name: $card_name,
+                        card_email: $card_email,
+                        card_phone: $card_phone,
+                        card_intro: $card_intro,
+                        card_photo: $card_photo,
                         created_at: date($created_at)
                     })
                 """, **data)
@@ -279,7 +279,7 @@ class CardInfoView(views.APIView):
 
         with driver.session() as session:
             # card_uid를 사용하여 카드 정보를 검색
-            result = session.run("MATCH (card:Card) WHERE card.uid = $card_uid RETURN card", card_uid=card_uid)
+            result = session.run("MATCH (card:CARD) WHERE card.card_uid = $card_uid RETURN card", card_uid=card_uid)
             record = result.single()
             if record:
                 card_info = record['card']
@@ -290,7 +290,6 @@ class CardInfoView(views.APIView):
                         "card_email": card_info['card_email'],
                         "card_phone": card_info['card_phone'],
                         "card_intro": card_info['card_intro'],
-                        "card_photo": card_info['card_photo']
                     }
                 }
             else:
@@ -299,6 +298,7 @@ class CardInfoView(views.APIView):
                     "result": None
                 }
             return Response(res, status=status.HTTP_200_OK)
+
 
 #캬드 정보 수정하기
 class CardUpdateView(views.APIView):
