b478975552c6613807deb5d137255973b2b129f1
hajaemin <penloo@naver.com>
2023-07-28 21:04:02 +0900

    feat: api 연동 # fix: 버그 수정 # docs: 문서 수정 test: 코드 추가 # refact: 코드 리팩토링 # style: 코드 formatting, 세미콜론 누락, 코드 자체의 변경이 없는 경우 # chore: 패키지 매니저 수정, 그 외 기타 수정 ex) .gitignore # design: CSS 등 사용자 UI 디자인 변경  comment: 주석 처리완료 # rename: 파일 또는 폴더 명을 수정하거나 옮기는 작업만인 경우 # remove: 파일을 삭제하는 작업만 수행한 경우 # !BREAKING CHANGE: 커다란 API 변경의 이유 # !HOTFIX: 급하게 치명적인 버그를 고쳐야 하는 경우

--- neo_db/urls.py
+++ neo_db/urls.py
@@ -1,18 +1,27 @@
 # backend/neo_db/urls.py
 from django.urls import path
 from .views import RegisterView, LoginView, UserInfoView, UserUpdateView, UpdateUserPhotoView, CardAddView, CardUpdateView ,UserRelationView, AllRelationView, CardDetailView,PhoneInfoView, CardInfoView, UpdateCardPhotoView
+
+
 urlpatterns = [
+    # 차례대로 회원가입 , 로그인
     path('users/register/', RegisterView.as_view()),
     path('users/login/', LoginView.as_view()),  # 추가한 줄
+
+    # 유저 정보 불러오기 , 유저 정보 수정 , 유저 프로필 사진 수정
     path('users/info/<str:user_uid>/', UserInfoView.as_view()),
     path('users/update/<str:user_uid>/', UserUpdateView.as_view()),
     path('users/photo/<str:user_uid>/', UpdateUserPhotoView.as_view()),  # <int:user_id> 부분에 실제 user_id를 넣습니다
+
+    # 명함 등록 , 명함 정보 불러오기 ,명함 정보 수정, 명함 프로필 사진 수정
     path('cards/add/', CardAddView.as_view()),
     path('cards/info/<str:user_uid>/', CardInfoView.as_view()),
     path('cards/update/<str:user_uid>/', CardUpdateView.as_view()),
+    path('cards/photo/<str:user_uid>/', UpdateCardPhotoView.as_view()),  # <int:user_id> 부분에 실제 user_id를 넣습니다
+
+    # 유저 관계 연결 , 관계 전체 보기 , 명함 관계 상세 보기 , 번호 조회하기
     path('relations/user/', UserRelationView.as_view(), name='create-relation'),
     path('relations/all/<str:user_uid>/', AllRelationView.as_view()),
-    path('cards/photo/<str:user_uid>/', UpdateCardPhotoView.as_view()),  # <int:user_id> 부분에 실제 user_id를 넣습니다
     path('relations/detail/<str:card_uid>/', CardDetailView.as_view(), name='card-detail'),
     path('relations/phone/<str:user_phone>/', PhoneInfoView.as_view()),
     #path('relations/nonuser', NonUserRegisterView.as_view(), name='nonuser-relation'),
